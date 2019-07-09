ddd47d5b008b37b23d8d3bed89584fb189a0c1fd
zxq <zhangxiaoqiang@panda-fintech.com>
2019-07-09 16:40:58 +0800

    result code

--- src/main/java/com/plt/base/common/ResultEnum.java
+++ src/main/java/com/plt/base/common/ResultEnum.java
@@ -4,11 +4,11 @@ package com.plt.base.common;
  * @author zxq
  */
 public enum ResultEnum {
-    OK(10000, "SUCCESS"),
-    AUTH_FAIL(20001, "权限不足"),
-    ILLEGAL_PARAM(40001, "非法参数"),
-    BUSINESS_FAIL(40002, "业务处理失败"),
-    SYSTEM_EXCEPTION_ERROR(50000, "系统异常"),
+    OK(200, "SUCCESS"),
+    AUTH_FAIL(400, "权限不足"),
+    ILLEGAL_PARAM(501, "非法参数"),
+    BUSINESS_FAIL(502, "业务处理失败"),
+    SYSTEM_EXCEPTION_ERROR(503, "系统异常"),
     ;
 
     private int code;
