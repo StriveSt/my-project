14093566812162c7ce224af9225d94f8a74a3843
zxq <zhangxiaoqiang@panda-fintech.com>
2019-07-15 20:24:57 +0800

    添加banner

--- src/main/java/com/plt/base/banner/PltBanner.java
+++ src/main/java/com/plt/base/banner/PltBanner.java
@@ -14,22 +14,22 @@ public class PltBanner implements Banner {
 
     private static final String[] BANNER =
             {
-                    "  ____    _   _   _          _      _   _   _____ ",
-                    " |  _ \\  | | | | | |        / \\    | \\ | | |_   _|",
-                    " | |_) | | | | | | |       / _ \\   |  \\| |   | |  ",
-                    " |  __/  | |_| | | |___   / ___ \\  | |\\  |   | |  ",
-                    " |_|      \\___/  |_____| /_/   \\_\\ |_| \\_|   |_|"
+                    "  ____            _                   _   ",
+                    " |  _ \\   _   _  | |   __ _   _ __   | |_ ",
+                    " | |_) | | | | | | |  / _` | | '_ \\  | __|",
+                    " |  __/  | |_| | | | | (_| | | | | | | |_ ",
+                    " |_|      \\__,_| |_|  \\__,_| |_| |_|  \\__|",
             };
 
     private static final String SPRING_BOOT = " :: Base :: ";
 
     @Override
     public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
-        out.println(AnsiOutput.toString(AnsiColor.BRIGHT_RED, BANNER[0]));
+        out.println(AnsiOutput.toString(AnsiColor.BRIGHT_BLUE, BANNER[0]));
         out.println(AnsiOutput.toString(AnsiColor.BRIGHT_GREEN, BANNER[1]));
         out.println(AnsiOutput.toString(AnsiColor.BRIGHT_YELLOW, BANNER[2]));
-        out.println(AnsiOutput.toString(AnsiColor.BRIGHT_BLUE, BANNER[3]));
-        out.println(AnsiOutput.toString(AnsiColor.BRIGHT_MAGENTA, BANNER[4]));
+        out.println(AnsiOutput.toString(AnsiColor.BRIGHT_MAGENTA, BANNER[3]));
+        out.println(AnsiOutput.toString(AnsiColor.BRIGHT_RED, BANNER[4]));
         out.println(AnsiOutput.toString(AnsiColor.GREEN, SPRING_BOOT));
     }
 }
