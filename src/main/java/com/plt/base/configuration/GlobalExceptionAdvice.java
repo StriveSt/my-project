e4951dc20d348f7d5119e3c3b73091caf6b3bb67
zxq <zhangxiaoqiang@panda-fintech.com>
2019-07-19 09:44:01 +0800

    整理异常

--- src/main/java/com/plt/base/configuration/GlobalExceptionAdvice.java
+++ src/main/java/com/plt/base/configuration/GlobalExceptionAdvice.java
@@ -7,6 +7,7 @@ import com.plt.base.exception.BusinessException;
 import com.plt.base.exception.IllegalParamException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,30 +26,45 @@ public class GlobalExceptionAdvice {
      */
     @ExceptionHandler(RuntimeException.class)
     public Result exHandler(RuntimeException ex) {
-        return forException(ResultEnum.SYSTEM_EXCEPTION_ERROR, ex);
+        log.error("系统错误：{}", ex.getMessage());
+        ex.printStackTrace();
+        return Result.generate(ResultEnum.SYSTEM_EXCEPTION_ERROR);
     }
 
+    /**
+     * 业务错误
+     */
     @ExceptionHandler(BusinessException.class)
     public Result bizExHandler(BusinessException ex) {
-        return forException(ResultEnum.BUSINESS_FAIL, ex);
+        log.info("业务错误：{}", ex.getMessage());
+        Result<Object> result = Result.generate(ResultEnum.BUSINESS_FAIL);
+        result.setMessage(ex.getMessage());
+        return result;
     }
 
+    /**
+     * 参数错误
+     */
     @ExceptionHandler({IllegalParamException.class, IllegalArgumentException.class, IllegalStateException.class})
     public Result argHandler(RuntimeException ex) {
-        return forException(ResultEnum.ILLEGAL_PARAM, ex);
+        log.info("参数错误：{}", ex.getMessage());
+        ex.printStackTrace();
+        return Result.generateSuccess(ResultEnum.ILLEGAL_PARAM);
     }
 
+    /**
+     * 授权失败
+     */
     @ExceptionHandler(AuthException.class)
     public Result authHandler(AuthException ex) {
-        return forException(ResultEnum.AUTH_FAIL, ex);
+        log.info("授权失败 {}", ex.getMessage());
+        return Result.generate(ResultEnum.AUTH_FAIL);
     }
 
-
-    private Result forException(ResultEnum e, Exception ex) {
-        log.error("异常信息：{}", ex.getMessage());
-        ex.printStackTrace();
-        Result<Object> r = Result.generate(e);
-        r.setMessage(ex.getMessage());
-        return r;
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Result methodException(HttpRequestMethodNotSupportedException ex) {
+        Result<Object> result = Result.generate(ResultEnum.ILLEGAL_PARAM);
+        result.setMessage("不支持的请求方法");
+        return result;
     }
 }
