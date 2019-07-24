00bea0a1d3cf6086f7eece78939c4e9f3b9cb4c4
zxq <zhangxiaoqiang@panda-fintech.com>
2019-07-24 11:48:44 +0800

    banner

--- src/main/java/com/plt/base/banner/PltBanner.java
+++ src/main/java/com/plt/base/banner/PltBanner.java
@@ -14,22 +14,24 @@ public class PltBanner implements Banner {
 
     private static final String[] BANNER =
             {
-                    "  ____            _                   _   ",
-                    " |  _ \\   _   _  | |   __ _   _ __   | |_ ",
-                    " | |_) | | | | | | |  / _` | | '_ \\  | __|",
-                    " |  __/  | |_| | | | | (_| | | | | | | |_ ",
-                    " |_|      \\__,_| |_|  \\__,_| |_| |_|  \\__|",
+                    "  ____            _                   _       ____                       \n",
+                    " |  _ \\   _   _  | |   __ _   _ __   | |_    | __ )    __ _   ___    ___ \n",
+                    " | |_) | | | | | | |  / _` | | '_ \\  | __|   |  _ \\   / _` | / __|  / _ \\\n",
+                    " |  __/  | |_| | | | | (_| | | | | | | |_    | |_) | | (_| | \\__ \\ |  __/\n",
+                    " |_|      \\__,_| |_|  \\__,_| |_| |_|  \\__|   |____/   \\__,_| |___/  \\___|\n"
             };
 
     private static final String PLT_BASE = " :: Base :: ";
 
     @Override
     public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+        out.println();
         out.println(AnsiOutput.toString(AnsiColor.BRIGHT_BLUE, BANNER[0]));
         out.println(AnsiOutput.toString(AnsiColor.BRIGHT_GREEN, BANNER[1]));
         out.println(AnsiOutput.toString(AnsiColor.BRIGHT_YELLOW, BANNER[2]));
         out.println(AnsiOutput.toString(AnsiColor.BRIGHT_MAGENTA, BANNER[3]));
         out.println(AnsiOutput.toString(AnsiColor.BRIGHT_RED, BANNER[4]));
         out.println(AnsiOutput.toString(AnsiColor.GREEN, PLT_BASE));
+        out.println();
     }
 }
