71384a4f1a6e538d1293df18b818edde247d46a7
xiaohongbin.it@pulant.com <xhb!@#2119>
2019-09-26 11:40:50 +0800

    增加ServiceException 定义 扩展BaseException 方法

--- src/main/java/com/plt/base/exception/BaseException.java
+++ src/main/java/com/plt/base/exception/BaseException.java
@@ -8,8 +8,13 @@ public abstract class BaseException extends RuntimeException {
     public BaseException() {
         super();
     }
-
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
     public BaseException(String message) {
         super(message);
+    }
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
