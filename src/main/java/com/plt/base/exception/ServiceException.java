da428898efa92bfeb113b60ae663cd9c4bd00253
xiaohongbin.it@pulant.com <xhb!@#2119>
2019-09-26 11:42:29 +0800

    增加ServiceException 定义描述

--- src/main/java/com/plt/base/exception/ServiceException.java
+++ src/main/java/com/plt/base/exception/ServiceException.java
@@ -1,5 +1,9 @@
 package com.plt.base.exception;
 
+
+/**
+ * 服务异常定义,封装服务调用异常
+ */
 public class ServiceException extends BaseException {
     public ServiceException(String message) {
         super(message);
